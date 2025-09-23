63e399279bd5c004bec1ad0d0f73f85cb1f8e6ba
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-09-23 14:53:35 +0200

    Movements and collision removed

--- main.py
+++ main.py
@@ -23,26 +23,6 @@ player_img = pygame.transform.scale(player_img, (SIZE, SIZE))  # Redimensionner 
 speed = 0
 speed_meter = my_font.render('Speed : 0', False, (0, 0, 0))
 
-# --- Murs ---
-walls = []
-
-def clamp(r):
-    r.left = max(0, r.left); r.top = max(0, r.top)
-    r.right = min(WIDTH, r.right); r.bottom = min(HEIGHT, r.bottom)
-
-def move_and_collide(r, dx, dy, speed, dt, walls):
-    # X
-    r.x += int(dx * speed * dt)
-    for w in walls:
-        if r.colliderect(w):
-            if dx > 0:  r.right = w.left
-            elif dx < 0: r.left  = w.right
-    # Y
-    r.y += int(dy * speed * dt)
-    for w in walls:
-        if r.colliderect(w):
-            if dy > 0:  r.bottom = w.top
-            elif dy < 0: r.top   = w.bottom
 
 # --- Boucle principale ---
 running = True
@@ -54,17 +34,10 @@ while running:
 
     # INPUT
     k = pygame.key.get_pressed()
-    dx = (k[pygame.K_RIGHT] or k[pygame.K_d]) - (k[pygame.K_LEFT] or k[pygame.K_q])
-    dy = (k[pygame.K_DOWN] or k[pygame.K_s]) - (k[pygame.K_UP] or k[pygame.K_z])
 
-    # LOGIQUE
-    move_and_collide(player, dx, dy, SPEED, dt, walls)
-    clamp(player)
 
     # DESSIN
     screen.fill(BG)
-    for w in walls:
-        pygame.draw.rect(screen, WCOLOR, w)
     screen.blit(player_img, player.topleft)  # On dessine l’image à la position du joueur
     screen.blit(speed_meter, (0, 0))
     pygame.display.flip()
