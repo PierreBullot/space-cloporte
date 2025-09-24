0ec76707f1157a2f10c37b89f3067f4151991f0a
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-09-24 10:07:12 +0200

    Renamed player.py to character.py, created player object in main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-import player
+import character
 import config
 
 
@@ -11,7 +11,9 @@ clock = pygame.time.Clock()
 
 
 player_rectangle = pygame.Rect(0 + SIZE // 2, HEIGHT // 2 - SIZE // 2, SIZE, SIZE)
-
+player_sprite = pygame.image.load("./images/player.jpg").convert_alpha()
+player_sprite = pygame.transform.scale(player_sprite, (SIZE, SIZE))  # Redimensionner à la taille du joueur
+player = character.Player(player_rectangle, player_sprite)
 
 # --- Boucle principale ---
 running = True
@@ -22,14 +24,14 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_f:
-                player.change_speed()
+                player.update_speed()
 
-    player.move_player()
+    player.rotate()
     player.handle_cooldown()
 
     # DESSIN
     screen.fill(BG)
-    screen.blit(player.rotated_player_img, player.player_position)  # On dessine l’image à la position du joueur
+    screen.blit(player.rotated_sprite, player.rotated_position)  # On dessine l’image à la position du joueur
     screen.blit(player.speed_meter, (0, 0))
     pygame.display.flip()
 
