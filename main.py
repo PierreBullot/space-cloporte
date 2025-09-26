ff28cef94bbee69dc9bcae6348cfd1367a9a81e8
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-09-26 15:37:36 +0200

    Added name to sound barrier

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ import obstacles
 pygame.init()
 pygame.font.init()
 
-
+# --- Initialize screen, background and clock ---
 screen: pygame.SurfaceType = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
 background = scenery.MovingBackground()
-
 clock = pygame.time.Clock()
 
+# --- Initialize player ---
 player_rectangle = pygame.Rect(0 + config.PLAYER_SIZE // 2, config.SCREEN_HEIGHT // 2 - config.PLAYER_SIZE // 2, config.PLAYER_SIZE, config.PLAYER_SIZE)
 player_sprite = pygame.image.load(config.PLAYER_SPRITE_PATH).convert_alpha()
 player_sprite = pygame.transform.scale(player_sprite, (config.PLAYER_SIZE, config.PLAYER_SIZE))  # Redimensionner à la taille du joueur
@@ -22,6 +22,7 @@ player = character.Player(player_rectangle, player_sprite)
 
 player.skills["dash"] = character.Dash(config.INITIAL_DASH_COOLDOWN)
 
+# --- Initialize interface ---
 main_font = pygame.font.SysFont(config.FONT_NAME, config.FONT_SIZE)
 secondary_font = pygame.font.SysFont(config.FONT_NAME, config.FONT_SIZE // 2)
 game_interface = interface.Interface(screen, main_font)
@@ -43,10 +44,11 @@ game_interface.interface_elements["dash_bar"] = interface.SkillStatus(player.ski
                                                                       config.DASH_BAR_HEIGHT
                                                                       )
 
+# --- Initialize obstacles ---
 sound_barrier_rectangle = pygame.Rect(config.SCREEN_WIDTH * 2, 0, config.SOUND_BARRIER_WIDTH, config.SCREEN_HEIGHT)
 sound_barrier_sprite = pygame.image.load(config.SOUND_BARRIER_SPRITE_PATH)
 sound_barrier_sprite = pygame.transform.scale(sound_barrier_sprite, (config.SOUND_BARRIER_WIDTH, config.SCREEN_HEIGHT))  # Redimensionner à la taille du mur
-sound_barrier = obstacles.SoundBarrier(sound_barrier_rectangle, sound_barrier_sprite, config.SOUND_BARRIER_SPEED)
+sound_barrier = obstacles.SoundBarrier(sound_barrier_rectangle, sound_barrier_sprite, config.SOUND_BARRIER_SPEED, main_font)
 
 
 # --- Boucle principale ---
--- obstacles.py
+++ obstacles.py
@@ -1,10 +1,23 @@
+import pygame
 
 
 class SoundBarrier:
-    def __init__(self, rectangle, sprite, base_speed):
+    def __init__(self, rectangle, sprite, base_speed, font):
         self.rectangle = rectangle
         self.sprite = sprite
         self.speed = base_speed
+        self.number = 1
+        self.font = font
+        self.text = f"MACH {self.number}"
+
+        self.update_sprite()
+
+    def update_sprite(self):
+        self.text = f"MACH {self.number}"
+        rendered_mach = self.font.render(self.text, False, "green")
+        rendered_mach = pygame.transform.rotate(rendered_mach, -90)
+        self.sprite.blit(rendered_mach, (self.rectangle.width / 2 - self.font.get_height() / 2, self.rectangle.height / 2 - self.font.get_linesize() / 2))
+
 
     def update_position(self, target_speed):
         self.rectangle.x = self.speed - target_speed
