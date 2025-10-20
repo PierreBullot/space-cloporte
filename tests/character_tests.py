c6a8530ff3aa3a716b971d16dcb8d3aef083255a
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-10-20 16:26:44 +0200

    Fichier character_tests.py created

---
+++ tests/character_tests.py
@@ -0,0 +1,29 @@
+import unittest
+import character
+import pygame
+import config
+
+pygame.init()
+screen: pygame.SurfaceType = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
+
+
+class PlayerTests(unittest.TestCase):
+    def test_update_max_speed(self):
+        try:
+            player_rectangle = pygame.Rect(0 + config.PLAYER_SIZE // 2, config.SCREEN_HEIGHT // 2 - config.PLAYER_SIZE // 2, config.PLAYER_SIZE, config.PLAYER_SIZE)
+            player_sprite = pygame.image.load(config.PLAYER_SPRITE_PATH).convert_alpha()
+            player_sprite = pygame.transform.scale(player_sprite, (config.PLAYER_SIZE, config.PLAYER_SIZE))  # Redimensionner à la taille du joueur
+            player = character.Player(player_rectangle, player_sprite)
+
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 0, "La vitesse maximale initiale devrait être 0")
+            self.assertEqual(player.max_speed_factor, 1, "Le facteur maximal initial devrait être 1")
+
+            player.speed = 20
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 20, "La vitesse maximale devrait être 20")
+            self.assertEqual(player.max_speed_factor, 1, "Le facteur maximal devrait être 1")
+            print("✅ Tous les tests Player.update_max_speed() sont OK")
+        except AssertionError as e:
+            print(f"❌ Erreur dans Player.update_max_speed() → {e}")
+            raise
