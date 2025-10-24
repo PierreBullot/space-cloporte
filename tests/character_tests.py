8da7e0f8e96242305c9ebe1ef0e49a6a7fb71af1
Pierre Bullot <pierre-bullot@hotmail.fr>
2025-10-24 09:26:54 +0200

    Tests for Player.update_max_speed() done.

--- tests/character_tests.py
+++ tests/character_tests.py
@@ -23,6 +23,46 @@ class PlayerTests(unittest.TestCase):
             player.update_max_speed()
             self.assertEqual(player.max_speed, 20, "La vitesse maximale devrait être 20")
             self.assertEqual(player.max_speed_factor, 1, "Le facteur maximal devrait être 1")
+
+            player.speed = 15
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 20, "La vitesse maximale devrait être 20")
+            self.assertEqual(player.max_speed_factor, 1, "Le facteur maximal devrait être 1")
+
+            player.speed = 500
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 500, "La vitesse maximale devrait être 500")
+            self.assertEqual(player.max_speed_factor, 1, "Le facteur maximal devrait être 1")
+
+            player.speed = 430583
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 430583, "La vitesse maximale devrait être 430583")
+            self.assertEqual(player.max_speed_factor, 1, "Le facteur maximal devrait être 1")
+
+            player.speed = 300
+            player.speed_factor = 1000
+            player.update_max_speed()   # 300 * 1000 = 300 000, which is < 430 583, so the max speed shouldn't be updated.
+            self.assertEqual(player.max_speed, 430583, "La vitesse maximale devrait être 430583")
+            self.assertEqual(player.max_speed_factor, 1, "Le facteur maximal devrait être 1")
+
+            player.speed = 500
+            player.speed_factor = 1000
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 500, "La vitesse maximale devrait être 500")
+            self.assertEqual(player.max_speed_factor, 1000, "Le facteur maximal devrait être 1000")
+
+            player.speed = 800
+            player.speed_factor = 1000000
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 800, "La vitesse maximale devrait être 800")
+            self.assertEqual(player.max_speed_factor, 1000000, "Le facteur maximal devrait être 1000000")
+
+            player.speed = 900
+            player.speed_factor = 1
+            player.update_max_speed()
+            self.assertEqual(player.max_speed, 800, "La vitesse maximale devrait être 800")
+            self.assertEqual(player.max_speed_factor, 1000000, "Le facteur maximal devrait être 1000000")
+
             print("✅ Tous les tests Player.update_max_speed() sont OK")
         except AssertionError as e:
             print(f"❌ Erreur dans Player.update_max_speed() → {e}")
